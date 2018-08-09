12034a3c8a8e4158e6b1cd9128afcc0cbb11161d
madanyli <madanyli@opendns.com>
2018-08-09 23:51:43 +0300

    First day

---
+++ presentations/day1/python-doc.py
@@ -0,0 +1,42 @@
+#Exp1
+year = 2017
+dec = 1.1
+hello = 'Hello, World!'
+alphabet = ['a', 'b', 'c']
+#EExp1
+#Exp2
+>> File 1 
+def fib(n):
+    if n == 0: return 0
+    elif n == 1: return 1
+    else: return fib(n - 1) + fib(n - 2)
+>> File 2 
+import fibonacci
+fibonacci.fib(30)
+832040
+#EExp2
+#Exp3 
+- None
+types.NoneType: The null object None
+- Numbers:
+int: Integer
+long: Arbitrary - precision integer
+float: Floating point
+complex: Complex number
+bool: Boolean(True or False)
+- Sequences:
+Character string: str
+Unicode character string: unicode
+Abstract base type for all strings: basestring
+list: List
+tuple: Tuple
+Returned by xrange(): xrange
+- Mapping:
+dict: Dictionary
+Sets: set
+Mutable set: frozenset
+... AND MUCH-MUCH MORE!!1
+#EExp3
+#Exp4
+python test.py
+#EExp4
